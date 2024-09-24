ed032f7f76dc84001ba983abb114601a9a948960
libinpeter <libinpeter13@gmail.com>
2024-09-24 23:24:35 +0530

    minor changes

--- instagram.js
+++ instagram.js
@@ -9,7 +9,7 @@ puppeteer.use(StealthPlugin());
 
     try {
         // Launch the browser in headless mode (can switch to false for debugging)
-        browser = await puppeteer.launch({ headless: true });
+        browser = await puppeteer.launch({ headless: false });
         const page = await browser.newPage();
 
         await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
